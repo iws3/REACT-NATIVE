1bdaae874972dd39ac744e160981dd6e08b10830
iws-seed <iwstechnical7@gmail.com>
2025-11-03 06:02:32 +0100

    dded to dataa

---
+++ data/EmpjieData.js
@@ -0,0 +1,30 @@
+const emojie = [
+  {
+    id: 1,
+    name: "good",
+    emojie: "👍",
+  },
+
+  {
+    id: 2,
+    name: "star",
+    emojie: "⭐",
+  },
+  {
+    id: 3,
+    name: "ball",
+    emojie: "⚽",
+  },
+
+  {
+    id: 4,
+    name: "girl",
+    emojie: "👧",
+  },
+
+  {
+    id: 5,
+    name: "engineer",
+    emojie: "👨‍🚒",
+  },
+];
